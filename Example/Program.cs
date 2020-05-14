340577a53eb43af82c531d7bfde960f4b21453cf
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-05-14 10:29:53 +1000

    Being

---
+++ Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace lbry.net
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Client client = new Client();
+            await client.GetAccountBalance();
+        }
+    }
+}
